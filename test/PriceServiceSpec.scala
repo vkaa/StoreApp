55ca312a5cfc9b720d33ff1f79cf41a9cfad8b2d
Victor Savkov <victor.savkov@intelliware.ca>
2017-11-22 16:15:56 -0500

    commented out prints from test

--- test/PriceServiceSpec.scala
+++ test/PriceServiceSpec.scala
@@ -27,16 +27,16 @@ class PriceServiceSpec extends PlaySpec {
       val svc: PriceService = new FlakyMockPriceService
       breakable {
         for (i <- 1 to 10) {
-//          println(s"svc.price attempt: $i")
+          //          println(s"svc.price attempt: $i")
           Try {
             Await.result(svc.price("133"), 1.milliseconds)
           } match {
             case Success(price) => {
-//              println(s"svc.price success: $price")
+              //              println(s"svc.price success: $price")
               price mustBe Some(133.33)
             }
             case Failure(e) => {
-//              println(s"svc.price failure: $e")
+              //              println(s"svc.price failure: $e")
               e.getMessage mustBe "Someone snipped the network cable!"
               break
             }
@@ -44,4 +44,5 @@ class PriceServiceSpec extends PlaySpec {
         }
       }
     }
+  }
 }
