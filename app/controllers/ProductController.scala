92aa2fa58074f1b22114409def89938018b9f198
Victor Savkov <victor.savkov@intelliware.ca>
2017-11-21 21:10:12 -0500

    added ProductController.product stub

--- app/controllers/ProductController.scala
+++ app/controllers/ProductController.scala
@@ -1,12 +1,17 @@
 package controllers
 
-import javax.inject.Singleton
+import javax.inject.{Inject, Singleton}
 
 import model.ProductId
 import play.api.mvc._
+import play.twirl.api.HtmlFormat
+
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class ProductController extends Controller {
+class ProductController @Inject() ()(implicit executionContext:ExecutionContext) extends Controller {
 
-  def product(productId: ProductId) = ???
+  def product(productId: ProductId) = Action.async {
+    Future.successful(Ok(HtmlFormat.raw("<h1>ProductController.product stub</h1>")))
+  }
 }
