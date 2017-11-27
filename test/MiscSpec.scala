c47676e0585e2ab109c18197a6ddecefaf74667f
Victor Savkov <victor.savkov@intelliware.ca>
2017-11-27 17:37:15 -0500

    WIP - misc tests to understand Future.failed

--- test/MiscSpec.scala
+++ test/MiscSpec.scala
@@ -4,9 +4,10 @@ import scala.concurrent.{Await, Future}
 import util.Misc.tryToFutureEither
 
 import scala.concurrent.duration._
-import scala.util.Try
-
+import scala.util.{Failure, Success, Try}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import scala.util.control.NonFatal
 
 class MiscSpec extends PlaySpec{
 
@@ -22,16 +23,64 @@ class MiscSpec extends PlaySpec{
       futValue.right.get mustBe 7
     }
 
+    "test Exception on NonFatal" in {
+      println(s"Exception NonFatal: ${NonFatal(new Exception("Someone snipped the network cable!"))}")
+    }
+
+    "test Exception on NonFatal with Try of Success" in {
+      try Success(throw new Exception("Someone snipped the network cable!"))
+      catch {
+         case NonFatal(e) => println(s"NonFatal '${e.getMessage}'")
+         case _: Throwable => println("FATAL")
+      }
+    }
+
+    "test Exception on NonFatal with Try" in {
+      try { throw new Exception("Someone snipped the network cable!") }
+      catch {
+        case NonFatal(e) => println(s"NonFatal '${e.getMessage}'")
+        case e: Throwable => println(s"Throwable '${e.getMessage}'")
+      }
+    }
+
     def futFailed(v: BigDecimal): Future[BigDecimal] =
       Future.failed(new Exception("Someone snipped the network cable!"))
+
+    "test try Await.result" in {
+      try {
+        val ret = Await.result(futFailed(7), 1 .seconds)
+        println(s"Await.result(futFailed(7), 1 .seconds): ${ret}")
+      }
+      catch {
+        case NonFatal(e) => println(s"NonFatal '${e.getMessage}'")
+        case e: Throwable => println(s"Throwable '${e.getMessage}'")
+        case _ => println(s"case _")
+      }
+    }
+
+    "test Try Await.result match" in {
+      Try { Await.result(futFailed(7), 1 .seconds) } match {
+        case Success(r) => println(s"Success '${r}'")
+        case Failure(r) => println(s"Failure '${r}'")
+      }
+    }
+
+    "test Await.result value" in {
+      val v = Await.result(futFailed(7), 1 .seconds)
+      println(s"Await.result: ${v.getClass}")
+    }
 
     "return Left for failed future" in {
 //      val futEither = futFailed(7).toFutureEither
       val futEither: Future[Either[String, BigDecimal]] = for {
         x <- futFailed(7)
+//        x <- futFailed(7).recoverWith({case e: Exception => Left(e.getMessage)})
         y = Try {
           x
         }.map(Right(_)).recover({case e: Exception => Left(e.getMessage)}).get
+//        }.map(Right(_)).recoverWith({case e: Exception => Left(e.getMessage)}).get
+//        }.recover({case e: Exception => Left(e.getMessage)}).get
+//        }.recoverWith({case e: Exception => Left(e.getMessage)}).get
         /*
         y <- Try {
           futFailed(7)
